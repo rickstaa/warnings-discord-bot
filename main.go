a2dd0ffab31e77b9e6eadd9bfd22d85c4bd5a179
Rick Staa <rick.staa@outlook.com>
2024-01-08 11:11:02 +0100

    refactor: add comments

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Config represents the configuration for the bot.
 type Config struct {
 	Token        string `json:"Token"`
 	KeywordLists []struct {
@@ -24,36 +25,35 @@ type Config struct {
 
 func main() {
 	fmt.Println("Starting bot Warnings Bot...")
-
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
+	// Parse config file.
 	configFile, err := os.ReadFile("config/config.json")
 	if err != nil {
 		log.Fatalf("Error reading config file: %v", err)
 	}
-
 	var config Config
 	if err := json.Unmarshal(configFile, &config); err != nil {
 		log.Fatalf("Error parsing config JSON: %v", err)
 	}
-
 	for i, keywordList := range config.KeywordLists {
 		keywordsPattern := `(?i)\b(` + strings.Join(keywordList.Keywords, "|") + `)\b`
 		config.KeywordLists[i].KeywordRegex = regexp.MustCompile(keywordsPattern)
 	}
 
+	// Retrieve bot token.
 	token := os.Getenv("DISCORD_BOT_TOKEN")
 	if token == "" {
 		token = config.Token
 	}
-
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatalf("Error creating Discord session: %v", err)
 	}
 
+	// Add handler for messages.
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.ID == s.State.User.ID {
 			return
@@ -78,12 +78,13 @@ func main() {
 		}
 	})
 
+	// Start the bot.
 	err = dg.Open()
 	if err != nil {
 		log.Fatalf("Error opening connection to Discord: %v", err)
 	}
-
 	fmt.Println("Bot is now running. Press CTRL+C to exit.")
 
+	// Block the main goroutine until a termination signal is received (CTRL+C).
 	select {}
 }
