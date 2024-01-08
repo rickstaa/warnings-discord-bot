rickstaa/warnings-discord-bot

commit 0a5f6615ac3b4a13cd47cb75499818df1a7aa354

feat: add 'excluded_roles' config parameter (#9)
This commit adds the 'excluded_roles config parameter. This parameter
can be used to ensure warnings will not be thrown when a user has a
certain role.

main.go
@@ -19,6 +19,7 @@ type Config struct {
 		WarningMessage       string   `json:"warning_message"`
 		ExternalLinkRequired bool     `json:"external_link_required"`
 		RequiredRoles        []string `json:"required_roles"`
+		ExcludedRoles        []string `json:"excluded_roles"`
 		KeywordRegex         *regexp.Regexp
 	} `json:"keyword_lists"`
 }
@@ -73,7 +74,7 @@ func main() {
 			}
 
 			// Check if RequiredRoles is empty or if the user has one of the required roles
-			if len(keywordList.RequiredRoles) == 0 {
+			if len(keywordList.RequiredRoles) == 0 && len(keywordList.ExcludedRoles) == 0 {
 				// If RequiredRoles is empty, the bot responds to all messages
 				// Continue checking for keywords and sending warnings
 			} else {
@@ -84,7 +85,7 @@ func main() {
 					continue
 				}
 
-				// Check if the message sender has any of the required roles
+				// Check if the message sender has any of the required roles.
 				hasRequiredRole := false
 				for _, roleID := range member.Roles {
 					role, err := s.State.Role(m.GuildID, roleID)
@@ -93,7 +94,14 @@ func main() {
 						continue
 					}
 
-					// Check if the role name matches any of the required roles
+					// Check if the role name matches any of the excluded roles.
+					for _, excludedRole := range keywordList.ExcludedRoles {
+						if role.Name == excludedRole {
+							return // User has an excluded role, skip this message.
+						}
+					}
+
+					// Check if the role name matches any of the required roles.
 					for _, requiredRole := range keywordList.RequiredRoles {
 						if role.Name == requiredRole {
 							hasRequiredRole = true
@@ -101,23 +109,23 @@ func main() {
 						}
 					}
 					if hasRequiredRole {
-						break // User has a required role, no need to check further
+						break // User has a required role, no need to check further.
 					}
 				}
 
-				// If the user doesn't have any of the required roles, skip this message
+				// If the user doesn't have any of the required roles, skip this message.
 				if !hasRequiredRole {
 					continue
 				}
 			}
 
-			// Create a regular expression pattern dynamically for all keywords in the list with case-insensitivity
+			// Create a regular expression pattern dynamically for all keywords in the list with case-insensitivity.
 			keywordsPattern := `(?i)\b(` + strings.Join(keywordList.Keywords, "|") + `)\b`
 			dmRegex := regexp.MustCompile(keywordsPattern)
 
-			// Check if the message contains any of the specified keywords dynamically
+			// Check if the message contains any of the specified keywords dynamically.
 			if dmRegex.MatchString(content) {
-				// Reply with the warning message
+				// Reply with the warning message.
 				_, err := s.ChannelMessageSendReply(m.ChannelID, keywordList.WarningMessage, m.Reference())
 				if err != nil {
 					log.Printf("Error sending message: %v", err)

commit 5079d2edaf16330d3b010f541207b4f0ad8e6d34

fix: fix no roles condition
This commit ensures a warning message is thrown when both the
`required_roles` and `excluded_roles` config variables are empty. This
functionality has been broken since
https://github.com/rickstaa/warnings-discord-bot/pull/9.

main.go
@@ -73,50 +73,45 @@ func main() {
 				continue // Skip if external link is required but not found
 			}
 
-			// Check if RequiredRoles is empty or if the user has one of the required roles
-			if len(keywordList.RequiredRoles) == 0 && len(keywordList.ExcludedRoles) == 0 {
-				// If RequiredRoles is empty, the bot responds to all messages
-				// Continue checking for keywords and sending warnings
-			} else {
-				// Fetch the roles of the message sender
-				member, err := s.GuildMember(m.GuildID, m.Author.ID)
+			// Fetch the roles of the message sender
+			member, err := s.GuildMember(m.GuildID, m.Author.ID)
+			if err != nil {
+				log.Printf("Error fetching member: %v", err)
+				continue
+			}
+
+			// Initialize flags for role checks
+			hasRequiredRole := len(keywordList.RequiredRoles) == 0
+			hasExcludedRole := false
+			for _, roleID := range member.Roles {
+				role, err := s.State.Role(m.GuildID, roleID)
 				if err != nil {
-					log.Printf("Error fetching member: %v", err)
+					log.Printf("Error fetching role: %v", err)
 					continue
 				}
 
-				// Check if the message sender has any of the required roles.
-				hasRequiredRole := false
-				for _, roleID := range member.Roles {
-					role, err := s.State.Role(m.GuildID, roleID)
-					if err != nil {
-						log.Printf("Error fetching role: %v", err)
-						continue
+				// Check against excluded roles
+				for _, excludedRole := range keywordList.ExcludedRoles {
+					if role.Name == excludedRole {
+						hasExcludedRole = true
+						break
 					}
+				}
 
-					// Check if the role name matches any of the excluded roles.
-					for _, excludedRole := range keywordList.ExcludedRoles {
-						if role.Name == excludedRole {
-							return // User has an excluded role, skip this message.
-						}
-					}
-
-					// Check if the role name matches any of the required roles.
+				// Check against required roles
+				if !hasRequiredRole {
 					for _, requiredRole := range keywordList.RequiredRoles {
 						if role.Name == requiredRole {
 							hasRequiredRole = true
 							break
 						}
 					}
-					if hasRequiredRole {
-						break // User has a required role, no need to check further.
-					}
 				}
+			}
 
-				// If the user doesn't have any of the required roles, skip this message.
-				if !hasRequiredRole {
-					continue
-				}
+			// Skip this message if the user has an excluded role or does not have a required role
+			if hasExcludedRole || !hasRequiredRole {
+				continue
 			}
 
 			// Create a regular expression pattern dynamically for all keywords in the list with case-insensitivity.